0778f13ac1ea970d9c9102734187e63474dd28f9
Zhenwei Wang <zhenwei.wang.1990@gmail.com>
2023-04-12 09:54:17 +0800

    feat: extract json keys

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,19 @@
-import Index from "./index.vue";
-export default Index;
+import { textTransformTool } from "@he3-kit/utils";
+
+const EXAMPLE_STR = `{
+    "abc": "bcd",
+    "hello": "world"
+}
+`;
+
+const extractKeys = (str) => {
+  try {
+    const jsonObj = JSON.parse(str);
+    return Object.keys(jsonObj).join(",");
+  } catch {}
+};
+
+export default textTransformTool({
+  inputHandler: extractKeys,
+  sampleData: EXAMPLE_STR,
+});
